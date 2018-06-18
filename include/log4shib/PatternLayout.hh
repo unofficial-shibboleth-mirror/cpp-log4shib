bf352a35c708a4ea8ca4bcb5e59b362cf88b85ef
Scott Cantor <cantor.2@osu.edu>
2018-06-18 15:53:32 -0400

    Incorporate some Debian patches.

--- include/log4shib/PatternLayout.hh
+++ include/log4shib/PatternLayout.hh
@@ -48,38 +48,36 @@ namespace log4shib {
         PatternLayout();
         virtual ~PatternLayout();
         
-        // NOTE: All double percentage signs ('%%') followed by a character
-        //       in the following comments should actually be a single char.
-        //       The doubles are included so that doxygen will print them correctly.
         /**
-         * Formats the LoggingEvent in the style set by
-		 * the setConversionPattern call. By default, set
-		 * to "%%m%%n"
+           Formats the LoggingEvent in the style set by
+           the setConversionPattern call. By default, set
+           to "%m%n"
          **/
         virtual std::string format(const LoggingEvent& event);
 
         /**
-         * Sets the format of log lines handled by this
-         * PatternLayout. By default, set to "%%m%%n".<br>
-         * Format characters are as follows:<br>
-         * <li><b>%%</b> - a single percent sign</li>
-         * <li><b>%%c</b> - the category</li>
-         * <li><b>%%d</b> - the date\n
-         *  Date format: The date format character may be followed by a date format 
-         *  specifier enclosed between braces. For example, %%d{%%H:%%M:%%S,%%l} or %%d{%%d %%m %%Y %%H:%%M:%%S,%%l}.
-         *  If no date format specifier is given then the following format is used:
-         *  "Wed Jan 02 02:03:55 1980". The date format specifier admits the same syntax 
-         *  as the ANSI C function strftime, with 1 addition. The addition is the specifier
-         *  %%l for milliseconds, padded with zeros to make 3 digits.</li>
-         * <li><b>%%m</b> - the message</li>
-         * <li><b>%%n</b> - the platform specific line separator</li>
-         * <li><b>%%p</b> - the priority</li>
-         * <li><b>%%r</b> - milliseconds since this layout was created.</li>
-         * <li><b>%%R</b> - seconds since Jan 1, 1970</li>
-         * <li><b>%%u</b> - clock ticks since process start</li>
-         * <li><b>%%x</b> - the NDC</li>
-         * @param conversionPattern the conversion pattern
-         * @exception ConfigureFailure if the pattern is invalid
+           Sets the format of log lines handled by this
+           PatternLayout. By default, set to "%m%n".
+           Format characters are as follows:
+           - <b>%%</b> - a single percent sign
+           - <b>%%c</b> - the category
+           - <b>%%d</b> - the date\n
+            Date format: The date format character may be followed by a date format
+            specifier enclosed between braces. For example, %%d{%%H:%%M:%%S,%%l} or %%d{%%d %%m %%Y %%H:%%M:%%S,%%l}.
+            If no date format specifier is given then the following format is used:
+            "Wed Jan 02 02:03:55 1980". The date format specifier admits the same syntax
+            as the ANSI C function strftime, with 1 addition. The addition is the specifier
+            %%l for milliseconds, padded with zeros to make 3 digits.
+           - <b>%%m</b> - the message
+           - <b>%%n</b> - the platform specific line separator
+           - <b>%%p</b> - the priority
+           - <b>%%r</b> - milliseconds since this layout was created
+           - <b>%%R</b> - seconds since Jan 1, 1970
+           - <b>%%u</b> - clock ticks since process start
+           - <b>%%x</b> - the NDC
+ 
+           @param conversionPattern the conversion pattern
+           @exception ConfigureFailure if the pattern is invalid
          **/
         virtual void setConversionPattern(const std::string& conversionPattern)
             throw(ConfigureFailure);
